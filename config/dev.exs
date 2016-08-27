e27acb7e21a577d966e97d01a391c1e067670fd8
Robert Boone <robert@rlb3.com>
2016-08-27 13:59:07 -0500

    Comment out postgres username and password

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :chat, Chat.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  # username: "postgres",
+  # password: "postgres",
   database: "chat_dev",
   hostname: "localhost",
   pool_size: 10
